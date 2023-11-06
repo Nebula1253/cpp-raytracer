0c2fa08a6b4088f204db6959883aaa5bad55d396
Neel Amonkar <studentneel@gmail.com>
2023-11-06 22:31:00 +0000

    Initial commit

---
+++ build/CMakeFiles/CGRCW2.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CGRCW2.exe"
+  "CGRCW2.exe.manifest"
+  "CGRCW2.pdb"
+  "CMakeFiles/CGRCW2.dir/Code/main.cpp.obj"
+  "CMakeFiles/CGRCW2.dir/Code/main.cpp.obj.d"
+  "libCGRCW2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CGRCW2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Neel Amonkar/OneDrive - University of Edinburgh/CG - Rendering/CGRCW2
+# Build directory: C:/Users/Neel Amonkar/OneDrive - University of Edinburgh/CG - Rendering/CGRCW2/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
