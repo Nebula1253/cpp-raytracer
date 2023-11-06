397308dfa625c4328f4fdfb0abd6e08f6128f81d
Neel Amonkar <studentneel@gmail.com>
2023-11-06 23:23:17 +0000

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Neel Amonkar/OneDrive - University of Edinburgh/CG - Rendering/CGRCW2
-# Build directory: C:/Users/Neel Amonkar/OneDrive - University of Edinburgh/CG - Rendering/CGRCW2/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
