22573816f34b84d6660bfb1b1889840f14b7a6cf
Neel Amonkar <studentneel@gmail.com>
2023-11-09 19:15:00 +0000

    AAAAAAAAAAAAAAAA

--- Code/sphere.h
+++ Code/sphere.h
@@ -26,10 +26,12 @@ class sphere : public shape {
             auto c = dot(oc, oc) - radius*radius;
             auto discriminant = b*b - 4*a*c; // the square root term in the quadratic formula
 
-            auto t1 = -b + sqrt(discriminant) / (2.0*a);
-            auto t2 = -b - sqrt(discriminant) / (2.0*a);
+            return (discriminant >= 0);
 
-            if (t1 < 0 && t2 < 0) return false; // both solutions are negative, meaning the object is BEHIND the camera, so who cares lol
-            else return true;
+            // auto t1 = -b + sqrt(discriminant) / (2.0*a);
+            // auto t2 = -b - sqrt(discriminant) / (2.0*a);
+
+            // if (t1 < 0 && t2 < 0) return false; // both solutions are negative, meaning the object is BEHIND the camera, so who cares lol
+            // else return true;
         }
 };
--- Code/triangle.h
+++ Code/triangle.h
@@ -17,31 +17,81 @@ class triangle : public shape {
         // point3* get_vertices() const { return vertices; }
         point3 get_vertex(int i) const { return vertices[i]; }
 
-        // TODO: intersection
-        bool intersection(const ray& r) const {
+        bool intersection_neel(const ray& r) const {
             vec3 edge1 = vertices[1] - vertices[0];
-            vec3 edge2 = vertices[2] - vertices[1];
+            vec3 edge2 = vertices[2] - vertices[0];
 
-            vec3 plane_normal = cross(edge1, edge2);
-            if (dot(plane_normal, r.direction())) {
+            vec3 plane_normal = -cross(edge1, edge2);
+            vec3 dir = r.direction();
+            if (dot(plane_normal, dir) == 0.0) {
                 return false; // the ray is parallel to the plane of the triangle, so how the hell would it ever intersect
             }
 
-            auto T = r.origin() - vertices[0];
-            auto e2 = vertices[2] - vertices[1];
+            // the scalar product is defined as dot(a, cross(b, c)) - can be seen as the determinant of the matrix [a, b, c]
+            auto O_minus_v1 = r.origin() - vertices[0];
+            auto det_a = dot(dir, plane_normal);
+            auto det_a1 = dot(O_minus_v1, plane_normal);
+            auto det_a2 = dot(dir, cross(O_minus_v1, edge2));
+            auto det_a3 = dot(dir, cross(edge2, O_minus_v1));
 
-            auto scalar = 1 / dot(cross(r.direction(), e2), edge1);
-            vec3 vector = vec3(dot(cross(T, edge1), e2), dot(cross(r.direction(), e2), T), dot(cross(T, edge1), r.direction()));
+            auto t = det_a1 / det_a;
+            auto u = det_a2 / det_a;
+            auto v = det_a3 / det_a;
 
-            vec3 tuv = (1/scalar) * vector;
-            double t = tuv.x();
-            double u = tuv.y();
-            double v = tuv.z();
+            // print t,u,v to std::cerr
+            std::cerr << "t: " << t << " u: " << u << " v: " << v << "\n";
 
-            return !(u < 0 || u > 1 || v < 0 || u+v > 1 || t <= 0);
+            return !(u < 0 || v < 0 || u+v > 1 || t <= 0);
+        }
 
-            // if (tuv.x() > 0) return true;
-            // else return false;
+        // stolen off wikipedia's page on the MT algorithm
+        bool intersection(const ray&r) const {
+            const float EPSILON = 0.000001;
+            vec3 vertex0 = vertices[0];
+            vec3 vertex1 = vertices[1];  
+            vec3 vertex2 = vertices[2];
+            vec3 edge1, edge2, h, s, q;
+            double a, f, u, v;
+            edge1 = vertex1 - vertex0;
+            edge2 = vertex2 - vertex0;
+
+            // h = r.direction().crossProduct(edge2);
+            h = cross(r.direction(), edge2);
+            // a = edge1.dotProduct(h);
+            a = dot(edge1, h);
+
+            if (a > -EPSILON && a < EPSILON)
+                return false;    // This ray is parallel to this triangle.
+
+            f = 1.0 / a;
+            s = r.origin() - vertex0;
+            // u = f * s.dotProduct(h);
+            u = f * dot(s,h);
+
+            if (u < 0.0 || u > 1.0)
+                return false;
+
+            // q = s.crossProduct(edge1);
+            q = cross(s, edge1);
+            // v = f * rayVector.dotProduct(q);
+            v = f * dot(r.direction(), q);
+
+            if (v < 0.0 || u + v > 1.0)
+                return false;
+
+            // At this stage we can compute t to find out where the intersection point is on the line.
+            // float t = f * edge2.dotProduct(q);
+            double t = f * dot(edge2, q);
+
+            if (t > EPSILON) // ray intersection
+            {
+                // outIntersectionPoint = rayOrigin + rayVector * t;
+                std::cerr << "YAY! t: " << t << " u: " << u << " v: " << v << "\n";
+                return true;
+            }
+            else // This means that there is a line intersection but not a ray intersection.
+                std::cerr << "t: " << t << " u: " << u << " v: " << v << "\n";
+                return false;
         }
 }; 
 
