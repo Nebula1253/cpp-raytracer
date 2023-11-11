76ab66bc619c9ea616e57dcc9d913acb63f95a01
Neel Amonkar <studentneel@gmail.com>
2023-11-11 22:24:18 +0000

    added json library

--- Code/main.cpp
+++ Code/main.cpp
@@ -9,6 +9,9 @@
 #include <vector>
 #include "shape.h"
 #include "scene.h"
+#include "json.hpp"
+using json = nlohmann::json;
+#include <fstream>
 
 #include <iostream>
 
@@ -20,37 +23,13 @@ color ray_color(const ray& r, scene& s) {
     return s.getBackgroundColor();
 }
 
-int main() {
-    
-    // // Image
-    // auto aspect_ratio = 16.0 / 9.0;
-    // int image_width = 400;
+int main(int argc, char *argv[]) {
+    // assuming the file name of the JSON file is passed as the first argument
+    std::string filepath = __FILE__;
+    filepath.erase(filepath.length() - 13);
 
-    // // Calculate the image height, and ensure that it's at least 1.
-    // int image_height = static_cast<int>(image_width / aspect_ratio);
-    // image_height = (image_height < 1) ? 1 : image_height;
-
-    // // Camera
-
-    // auto focal_length = 1.0;
-    // auto viewport_height = 2.0;
-    // auto viewport_width = viewport_height * (static_cast<double>(image_width)/image_height);
-    // auto camera_center = point3(0, 0, 0);
-
-    // // Calculate the vectors across the horizontal and down the vertical viewport edges.
-    // auto viewport_u = vec3(viewport_width, 0, 0);
-    // auto viewport_v = vec3(0, -viewport_height, 0);
-
-    // // Calculate the horizontal and vertical delta vectors from pixel to pixel.
-    // auto pixel_delta_u = viewport_u / image_width;
-    // auto pixel_delta_v = viewport_v / image_height;
-
-    // // Calculate the location of the upper left pixel.
-    // auto viewport_upper_left = camera_center
-    //                          + vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
-    // std::cerr << "viewport_upper_left: " << viewport_upper_left << "\n";
-    // auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
-
+    std::ifstream jsonFile(filepath + argv[1]);
+    json j = json::parse(jsonFile);
 
     camera cam = camera(400, static_cast<int>(400 / (16.0 / 9.0)), point3(0,0,0), vec3(0,0,1), vec3(0,1,0), 90, 1.0);
 
